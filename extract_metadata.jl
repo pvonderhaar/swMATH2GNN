5ddd91e045004baad252f4d32ad77fd80ccbaf2a
a.vonderhaar01 <a.vonderhaar01@stud.uni-goettingen.de>
2024-05-16 16:12:04 +0200

    tried more things to get a correct df

--- extract_metadata.jl
+++ extract_metadata.jl
@@ -1,19 +1,29 @@
-using JSON
+using JSON3
 using DataFrames
 using JSONTables
+using LinearAlgebra
+using Flatten
 
-json_files = readdir("data/")
+json_files = readdir("data")
 
-full_df = DataFrame()
-
+#df_list = []
+"""
 for file in json_files
-    json_string = read(file, )
+    json_string = read(string("data/", file), String)
     data = JSON.parse(json_string)
     df = DataFrame(data)
-    full_df = vcat(full_df, df)
+    t_df = DataFrame(Matrix(df)')
+    println(names(df))
+    push!(df_list, df)
 end
+"""
+#full_df = vcat(df_list)
+
+json_string = read("data/full_data.json", String)
+json_data = Flatten.flatten(json_string)
+data = jsontable(json_data)
+df = DataFrame(data)
 
 
-
-print(nrows(df))
+println(size(df))
 
--- packages.jl
+++ packages.jl
@@ -1,5 +1,7 @@
 import Pkg
 Pkg.add("EzXML")
 Pkg.add("JSON")
+Pkg.add("JSON3")
 Pkg.add("DataFrames")
 Pkg.add("JSONTables")
+Pkg.add("Flatten")
